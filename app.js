e4aed26b0e38853bd8685ddb83a2445e6e23eeb2
RyzenMu <muruga13in2022@gmail.com>
2024-10-16 05:52:04 +0530

    hashing

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 
 const app = express();
@@ -26,7 +27,7 @@ console.log(process.env.API_KEY);
 console.log(process.env.SECRET);
 const secret = process.env.SECRET;
 
-userSchema.plugin(encrypt, {secret:secret, encryptedFields: ['password']});
+// userSchema.plugin(encrypt, {secret:secret, encryptedFields: ['password']});
 
 const User = new mongoose.model("simple_password", userSchema);
 
@@ -45,7 +46,7 @@ app.get('/register', function(req, res){
 app.post('/register', async function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     try {
@@ -59,7 +60,7 @@ app.post('/register', async function(req, res) {
 
 app.post("/login", async function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     try {
         const foundUser = await User.findOne({ email: username });
